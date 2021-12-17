29e2a79ff9ef295bfab1ee082d45eb6adfb49e50
Luna Goodnight <luna.lee@nextfortune.tw>
2021-12-17 17:43:05 +0800

    [Add] init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
 
+const A4Wrapper = styled.div`
+  width: 100%;
+  max-width: 77rem;
+  background: #fff;
+`
+
+const Body = styled.div`
+  background: thistle;
+  display: flex;
+  justify-content: center;
+`
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(2rem + 2vmin);
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Body className="App">
+      <A4Wrapper>
+
+        <Header>Luna Goodnight</Header>
+      </A4Wrapper>
+
+
+
+    </Body>
   );
 }
 
