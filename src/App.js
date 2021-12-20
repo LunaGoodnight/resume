d231fcc9ca821ffbd7b09f0c7801cfb5506aa15e
Luna Goodnight <luna.lee@nextfortune.tw>
2021-12-20 14:53:22 +0800

    [Add] detail

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import styled from "styled-components";
+import Info from './images/a.jpg'
 
 const A4Wrapper = styled.div`
   width: 100%;
-  max-width: 77rem;
+  max-width: 90rem;
   background: #fff;
+  padding: 3rem;
 `
 
 const Body = styled.div`
@@ -16,7 +18,32 @@ const Header = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: calc(2rem + 2vmin);
+  font-size: calc(3rem + 2vmin);
+  color: teal;
+  padding-bottom: 2rem;
+`
+
+const PersonInfoBlock = styled.div`
+  display: flex;
+  gap: 2rem;
+  justify-content: center;
+  img {
+    width: 100%;
+  }
+`
+
+const PersonImage = styled.div`
+  width: 16rem;
+`
+
+const InfoDetail = styled.ul`
+  text-align: left;
+  list-style: none;
+  font-size: calc(1rem + 1vmin);
+  width: 60rem;
+  li {
+    padding-bottom: 1rem;
+  }
 `
 function App() {
   return (
@@ -24,6 +51,18 @@ function App() {
       <A4Wrapper>
 
         <Header>Luna Goodnight</Header>
+        <PersonInfoBlock>
+          <PersonImage>
+
+            <img src={Info} alt="info"/>
+          </PersonImage>
+          <InfoDetail>
+            <li>javaspringtea@gmail.com</li>
+            <li>Taipei, Taiwan</li>
+          </InfoDetail>
+
+        </PersonInfoBlock>
+
       </A4Wrapper>
 
 
