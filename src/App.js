5ae8c1e0124794be25ef86a23e52431d21d4073d
Luna Goodnight <luna.lee@nextfortune.tw>
2021-12-20 19:14:18 +0800

    [Add] booklist

--- src/App.js
+++ src/App.js
@@ -1,73 +1,22 @@
 import "./App.css";
-import styled from "styled-components";
 import Info from "./images/a.jpg";
+import {
+  Body,
+  A4Wrapper,
+  Title,
+  Link,
+  CompanyTitle,
+  Header,
+  InfoDetail,
+  PersonImage,
+  PersonInfoBlock,
+  SkillList,
+  WorkDetailItem,
+  BookList,
+} from "./styleConfig";
+import CleanCode from "./images/cleancode.jpg";
+import Refactoring from "./images/refact2.webp";
 
-const A4Wrapper = styled.div`
-  width: 100%;
-  max-width: 90rem;
-  background: #fff;
-  padding: 3rem 6rem;
-  display: flex;
-  gap: 3rem;
-  flex-direction: column;
-  font-size: 1.7rem;
-`;
-
-const Body = styled.div`
-  background: thistle;
-  display: flex;
-  justify-content: center;
-`;
-const Header = styled.header`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: calc(3rem + 2vmin);
-  color: teal;
-`;
-
-const PersonInfoBlock = styled.div`
-  display: flex;
-  gap: 2rem;
-
-  img {
-    width: 100%;
-  }
-`;
-
-const PersonImage = styled.div`
-  width: 16.9rem;
-`;
-
-const Link = styled.a`
-  color: dodgerblue;
-  cursor: pointer;
-`;
-const InfoDetail = styled.ul`
-  text-align: left;
-  list-style: none;
-  font-size: calc(1rem + 1vmin);
-  li {
-    padding-bottom: 2rem;
-    &:last-child {
-      padding-bottom: 0;
-    }
-  }
-`;
-
-const Title = styled.h3`
-  color: teal;
-  font-size: 2.2rem;
-  padding-bottom: 1rem;
-`;
-
-const WorkDetailItem = styled.ul`
-  list-style-position: inside;
-`;
-
-const CompanyTitle = styled.h4`
-  font-weight: bold;
-`;
 function App() {
   return (
     <Body className="App">
@@ -89,6 +38,7 @@ function App() {
             </li>
           </InfoDetail>
         </PersonInfoBlock>
+
         <div>
           <Title>Professional Summary</Title>
           <p>
@@ -100,6 +50,7 @@ function App() {
             excel as a Front-End Developer.
           </p>
         </div>
+
         <div>
           <Title>Work Experience</Title>
 
@@ -117,6 +68,43 @@ function App() {
               </li>
             </WorkDetailItem>
           </div>
+        </div>
+
+        <div>
+          <Title>Skills</Title>
+          <SkillList>
+            <li>3+ yrs of experience in developing JavaScript Applications</li>
+            <li>Developing web application with React.js, Css-In-Js</li>
+            <li>Experience with Selenium</li>
+            <li>Experience with C#, .NET</li>
+            <li>Understanding OOP</li>
+            <li>Understanding SQL</li>
+            <li>Experience in Git, GitHub, GitLab and Git flow.</li>
+            <li>Understanding Amazon Web Services</li>
+            <li>Understanding CI/CD</li>
+            <li>
+              Experience with containerization and orchestration tools (Docker
+              and Kubernetes)
+            </li>
+            <li>
+              Strong attention to detail on every line of code, every unit test,
+              and every commit message
+            </li>
+            <li>
+              Comfortable with rapid development cycles and tight schedules
+            </li>
+          </SkillList>
+        </div>
+        <div>
+          <Title>Books I read through again and again</Title>
+          <BookList>
+            <li>
+              <img src={CleanCode} alt="clean code" />
+            </li>
+            <li>
+              <img src={Refactoring} alt="" />
+            </li>
+          </BookList>
         </div>
       </A4Wrapper>
     </Body>
---
+++ src/styleConfig.js
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+
+export const A4Wrapper = styled.div`
+  width: 100%;
+  max-width: 90rem;
+  background: #fff;
+  padding: 3rem 6rem;
+  display: flex;
+  gap: 3rem;
+  flex-direction: column;
+  font-size: 1.7rem;
+`;
+
+export const Body = styled.div`
+  background: thistle;
+  display: flex;
+  justify-content: center;
+`;
+export const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(3rem + 2vmin);
+  color: teal;
+`;
+
+export const PersonInfoBlock = styled.div`
+  display: flex;
+  gap: 2rem;
+
+  img {
+    width: 100%;
+  }
+`;
+
+export const PersonImage = styled.div`
+  width: 16.9rem;
+`;
+
+export const Link = styled.a`
+  color: dodgerblue;
+  cursor: pointer;
+`;
+
+export const InfoDetail = styled.ul`
+  text-align: left;
+  list-style: none;
+  font-size: calc(1rem + 1vmin);
+  li {
+    padding-bottom: 2rem;
+    &:last-child {
+      padding-bottom: 0;
+    }
+  }
+`;
+
+export const Title = styled.h3`
+  color: teal;
+  font-size: 2.2rem;
+  padding-bottom: 1rem;
+`;
+
+export const WorkDetailItem = styled.ul`
+  list-style-position: inside;
+`;
+
+export const CompanyTitle = styled.h4`
+  font-weight: bold;
+`;
+
+export const SkillList = styled.ul`
+  list-style-position: inside;
+`;
+
+export const BookList = styled.ul`
+  display: flex;
+  list-style: none;
+  gap: 2rem;
+  li {
+    width: 13rem;
+    height: 17rem;
+    overflow: hidden;
+  }
+`;
